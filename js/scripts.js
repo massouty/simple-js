5c74ad68ea25123f252fdbe0db54a0340beaca18
YOUSEF ALMASSOUTY <107114846+massouty@users.noreply.github.com>
2022-06-29 01:11:30 +0200

    Update scripts.js
    If applied,this commit will have edited  pokemonlist in file (scripts.js)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 alert('Hallo World');
 let favoriteFood='Turkish Food';
 document.write(favoriteFood);
+
+let pokemonlist=[{name:'Bulbasaur',height:0.7,type:['grass','poison']},
+                 {name:'Butterfree',height:1.1,type:['grass','bug','flying']},
+                 {name:'machop',height:0.8,type:['fighting','bug','grass']}];
