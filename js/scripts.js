7e6cc331e49b29fbd29f7d9d9eead75002266867
massouty <massouty@outlook.com>
2022-06-30 22:21:11 +0200

    Update scripts.js
    If applied ,this commit will have written in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,6 @@ let pokemonlist=[{name:'Bulbasaur',height:0.7,type:['grass','poison']},
                  {name:'Butterfree',height:1.1,type:['grass','bug','flying']},
                  {name:'machop',height:0.8,type:['fighting','bug','grass']}];
 
+document.write(pokemonlist);
+
+
