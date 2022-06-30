ae7ea10a3a0084c9ffdf5df95d269ba4c75e19f8
massouty <massouty@outlook.com>
2022-06-30 22:41:53 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 alert('Hallo World');
-let favoriteFood='Turkish Food';
+let favoriteFood = 'Turkish Food';
 document.write(favoriteFood);
 
 // these  codes and orders for task 1.2
 
-let pokemonlist=[{name:'Bulbasaur',height:0.7,type:['grass','poison']},
-                 {name:'Butterfree',height:1.1,type:['grass','bug','flying']},
-                 {name:'machop',height:0.8,type:['fighting','bug','grass']}];
+let pokemonlist = [{ name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+{ name: 'Butterfree', height: 1.1, type: ['grass', 'bug', 'flying'] },
+{ name: 'machop', height: 0.8, type: ['fighting', 'bug', 'grass'] }];
 
 document.write(pokemonlist);
 
