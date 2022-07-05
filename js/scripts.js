501d81a31c9a6c81795808a948b02935f84a2fba
massouty <massouty@outlook.com>
2022-07-05 14:38:50 +0200

    Update scripts.js
    If applied , this commit will have edited (scripts.jsl) page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 alert('Hallo World');
+let favoriteFood = 'Turkish Food';
+document.write(favoriteFood);
+
+let pokemonlist = [{ name: 'Bulbasaur', height: 7, type: ['grass', 'poison'] },
+{ name: 'Butterfree', height: 11, type: ['grass', 'bug', 'flying'] },
+{ name: 'machop', height: 8, type: ['fighting', 'bug', 'grass'] }];
+
+for (let i = 0; i < pokemonlist.length; i++) {
+    if (pokemonlist[i].height < 8 && pokemonlist[i].height > 5) {
+        document.write(pokemonlist[i].name + " is small body/ ");
+    }
+    else if (pokemonlist[i].height < 11) {
+        document.write(pokemonlist[i].name + " is normal body/ ");
+    } else { document.write(pokemonlist[i].name + " is big body,wow!/"); }
+}
 
 
+let pokemon1 = "Bulbasaur(Height:7)";
+document.write(pokemon1);
+let pokemon2 = "Butterfree(Height:11)-wow , this is big";
+document.write(pokemon2);
+let pokemon3 = "machop(Height:7)";
+document.write(pokemon3);
