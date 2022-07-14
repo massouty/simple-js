c5cad240d7541620efb1c7948365f50567874155
massouty <massouty@outlook.com>
2022-07-14 04:17:01 +0200

    Update scripts.js
    If applied , this commit will have changed  pokemonlist from( for loop) to (forEach loop )

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let pokemonlist = [{ name: 'Bulbasaur', height: 7, type: ['grass', 'poison'] },
 { name: 'Butterfree', height: 11, type: ['grass', 'bug', 'flying'] },
 { name: 'machop', height: 8, type: ['fighting', 'bug', 'grass'] }];
 
-for (let i = 0; i < pokemonlist.length; i++) {
+/*for (let i = 0; i < pokemonlist.length; i++) {
     if (pokemonlist[i].height < 8 && pokemonlist[i].height > 5) {
         document.write(pokemonlist[i].name + " is small body/ ");
     }
@@ -14,7 +14,13 @@ for (let i = 0; i < pokemonlist.length; i++) {
         document.write(pokemonlist[i].name + " is normal body/ ");
     } else { document.write(pokemonlist[i].name + " is big body,wow!/"); }
 }
-
+*/
+pokemonlist.forEach(function(pokemon){if (pokemon.height < 8 && pokemon.height > 5) {
+    document.write(pokemon.name + " is small body/ ");
+}
+else if (pokemon.height < 11) {
+    document.write(pokemon.name + " is normal body/ ");
+} else { document.write(pokemon.name + " is big body,wow!/");}});
 
 let pokemon1 = "Bulbasaur(Height:7)";
 document.write(pokemon1);
